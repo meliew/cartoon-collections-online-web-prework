0fe19faec2a36f3a49bfa470e86ec55646893515
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-03 03:30:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
-    cheese_types.include?(array)
+    cheese_types.include?(cheese)
   end
 end
