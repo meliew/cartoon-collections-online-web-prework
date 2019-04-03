caf0239ea6839c8c973dc24bf0b19abdfdafb9b8
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-03 02:36:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,7 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
+  array.each_with_index do |dwarf|
+    puts "index. " " dwarf"
+
   # Your code here
 end
 
