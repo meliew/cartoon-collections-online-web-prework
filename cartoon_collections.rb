7b8a084e83ac0226c8057262efe0fa04cec12ab3
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-03 02:51:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,10 @@ end
 
   # Your code here
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.each do |item|
+    puts "#{item}!"
+  end  
 end
 
 def long_planeteer_calls# code an argument here
