864efcb464eb7fee7b26c09645267597cf860855
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-03 02:42:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
   indexplus = index + 1
-  array.each_with_index do |ind, dwarf|
+  array.each_with_index do |index, dwarf|
     puts "#{indexplus} . dwarf"
   end
 end
